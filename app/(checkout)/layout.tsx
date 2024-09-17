e54e5cfacb21d74c2207a3e2d741e2448da6979c
hakku-is <yhakku@yandex.ru>
2024-09-18 00:54:14 +0400

    change suspense

--- app/(checkout)/layout.tsx
+++ app/(checkout)/layout.tsx
@@ -10,14 +10,12 @@ export const metadata: Metadata = {
 export default function CheckoutLayout({ children }: { children: React.ReactNode }) {
   return (
     <main className="min-h-screen bg-[#F4F1EE]">
-        <Container>
-            <Header 
-                hasSearch={false} 
-                hasCart={false} 
-                className='border-gray-200'
-            />
-                {children}
-        </Container>
+      <Container>
+        <Suspense>
+          <Header hasSearch={false} hasCart={false} className="border-gray-200" />
+        </Suspense>
+        {children}
+      </Container>
     </main>
   );
 }
