6398582f56f8baabf3f72a3a0372f1e647ced167
hakku-is <yhakku@yandex.ru>
2024-09-18 01:12:45 +0400

    change Postgre db

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,8 @@
 import { authOptions } from '@/shared/constants';
 import NextAuth from 'next-auth';
 
+export const dynamic = 'force-dynamic';
+
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
