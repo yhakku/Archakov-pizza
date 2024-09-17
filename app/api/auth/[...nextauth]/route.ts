d476b43258c34fc2c8b47591a079dbb79a4d3585
hakku-is <yhakku@yandex.ru>
2024-09-18 01:32:02 +0400

    env update

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,6 @@
 import { authOptions } from '@/shared/constants';
 import NextAuth from 'next-auth';
 
-export const dynamic = 'force-dynamic';
-
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
